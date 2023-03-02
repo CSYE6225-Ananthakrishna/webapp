cf793f94a02960a2d39ef100873e742cf0bc903b
ananthakrishna-kalle-neu <kalle.a@northeastern.edu>
2023-03-02 02:04:13 -0500

    Assignment5

--- packer/tmp/node.sh
+++ packer/tmp/node.sh
@@ -1,10 +1,11 @@
 sudo yum update -y
-
+sudo yum upgrade -y
 
 
 sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo yum install -y nodejs
+
 
 
 
@@ -18,8 +19,6 @@ sudo npm install bcrypt
 
 sleep 10
 sudo mv /tmp/node.service /etc/systemd/system/node.service
-
-sudo systemctl daemon-reload
 
 sudo systemctl enable node.service
 
